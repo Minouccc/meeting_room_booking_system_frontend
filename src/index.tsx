39fbed25d9588b1ccaefdd399f561ad5befc6031
Minouccc <343723899@qq.com>
2024-06-25 17:29:39 +0800

    feat(用户管理模块): 用户端信息修改页面

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Register } from "./container/Register";
+import { UpdatePassword } from "./container/UpdatePassword";
+import { ErrorPage } from "./container/ErrorPage";
+import { Login } from "./container/Login";
+import { Index } from "./container/index";
+import { UpdateInfo } from "./container/UpdateInfo";
+
+const routes = [
+  {
+    path: "/",
+    element: <Index></Index>,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "update_info",
+        element: <UpdateInfo />,
+      },
+    ],
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+  {
+    path: "update_password",
+    element: <UpdatePassword />,
+  },
+];
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<RouterProvider router={router} />);
